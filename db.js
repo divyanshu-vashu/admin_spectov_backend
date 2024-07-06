d702d5dd2b920873eaac69a125bdacd8dc8f7a37
Divyanshu Singh <89933176+divyanshu-vashu@users.noreply.github.com>
2024-07-06 11:50:21 +0530

    ADD ENV db.js

--- db.js
+++ db.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
 
 module.exports = () => {
-	const connectionParams = {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	};
-	try {
-		mongoose.connect('mongodb+srv://codesharecode:BnLVp1eZmXIFVpV8@cluster0.7z8gmsh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', connectionParams);
-		console.log("Connected to database successfully");
-	} catch (error) {
-		console.log(error);
-		console.log("Could not connect database!");
-	}
+    const connectionParams = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    };
+    try {
+        mongoose.connect(process.env.MONGO_URI, connectionParams);
+        console.log("Connected to database successfully");
+    } catch (error) {
+        console.log(error);
+        console.log("Could not connect to the database!");
+    }
 };
